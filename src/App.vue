b2d6fdc1665905730f54dd46deb7d2904c6064b5
yangwl <yangwl@zonekey.com.cn>
2020-11-18 16:24:55 +0800

    feat:添加css艺术之美

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h3>属性函数attr()</h3>
+  <a class="hover-tips btn-1" href="#0" data-msg="点击登录">登录</a>
+  <!-- <a class="hover-tips btn-2" href="#0"></a> -->
+  <h3>单位计算函数calc()</h3>
+  <section class="ele-box">
+    测试文字测试文字测试
+    文字测试文字测试文字
+    测试文字测试文字测试文字
+    测试文字
+  </section>
+  <h3>条形加载条</h3>
+  <ul class="custom-loading">
+    <li v-for="(ele) in 6" :key="ele"></li>
+  </ul>
+  <h3>标签导航</h3>
+</template>
+<style lang="scss">
+   @import "./styles/index.scss"; 
+</style>
+<script>
+
+export default {
+  name: 'App',
+  components: {
+   
+  }
+}
+</script>
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    css:{
+        sass:{
+            additionalData:`
+            @import "@/styles/variables.scss";
+            `
+        }
+    }
+}
